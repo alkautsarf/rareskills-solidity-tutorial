23574b87fde25dec520fa85e19f3ef2356e7c814
alkautsarf <alkautsarf22@gmail.com>
2023-11-20 02:30:58 +0700

    completed nested mappings

--- practice-problems/NestedMapping/src/NestedMapping.sol
+++ practice-problems/NestedMapping/src/NestedMapping.sol
@@ -9,14 +9,18 @@ contract NestedMapping {
         4. use the 'returnNestedDetails' function to return the values of a nested data
     */
 
+    mapping(address => mapping(uint256 => bool)) public nestedBool;
+
     function setNestedDetails(address _addr, uint256 _num) public {
         // your code here
+        nestedBool[_addr][_num] = true;
     }
 
     function returnNestedDetails(
         address _addr,
         uint256 _num
     ) public view returns (bool) {
+        return nestedBool[_addr][_num];
         // your code here
     }
 }
