94b18133585fceb8958f4c6a654b5068aa107466
alkautsarf <alkautsarf22@gmail.com>
2023-11-22 02:56:51 +0700

    completed priceIsRight

--- practice-problems/PriceIsRight/src/PriceIsRight.sol
+++ practice-problems/PriceIsRight/src/PriceIsRight.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 contract PriceIsRight {
     /// @dev make this function revert unless exactly 1 ether is sent as value in the function call
     function buy() public payable {
+        require(msg.value == 1 ether, "Not 1 ether");
         /// your code here
     }
 }
