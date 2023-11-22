0bcc63a745b8818d40eeff4fce4966e3a0114bd3
alkautsarf <alkautsarf22@gmail.com>
2023-11-22 18:10:15 +0700

    added modifiers

---
+++ modifiers/src/Ownable.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Ownable {
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "onlyOwner");
+        _;
+    }
+
+    function changeOwner(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+}
+
+contract HoldFunds is Ownable {
+    
+    function withdrawFunds() public onlyOwner {
+        (bool ok, ) = owner.call{value: address(this).balance}("");
+        require(ok, "transfer failed");
+    }
+
+    receive() external payable {
+
+    }
+}
